31445b25292f93646dca4779a0330d59b4c84707
Qinyue <qinyue.liu@etu.unistra.fr>
2021-12-07 19:46:54 +0100

    csv done

---
+++ main.py
@@ -0,0 +1,33 @@
+import re
+
+# -------------------------------- donnée nécessaire -----------------------------
+liste = []
+h_aspire = ["hâbler", "hacher", "hachurer", "haïr", "haler", "hâler", "haleter", "hancher", "handicaper", "hanter", "happer", "haranguer", "harasser", "harceler", "harder", "harnacher", "harpailler", "harper", "harponner", "hasarder", "hâter", "haubaner", "hausser", "haver", "havir", "héler", "hercher", "hérisser", "hérissonner", "herscher", "herser", "heurter", "hisser", "hocher", "hongrer", "hongroyer", "honnir", "hoqueter", "hotter", "houblonner", "houer", "houpper", "hourder", "hourdir", "houspiller", "hucher", "huer", "hululer", "humer", "hurler"]
+
+
+def make_dictionary():
+    with open("dela-fr-public.dic", mode="r",encoding="UTF-16") as fd:
+        # -------------------------- Creation de fichier csv ----------------------
+        with open("dictionary.csv", mode="a", encoding="UTF-8") as output_f:
+            title = "lemme" + '\t' + "forme fléchie" + "\t" + "grammaire" + "\n"
+            output_f.write(title)
+
+        # -------------------------- Traitement de texte dans .dic ----------------
+            line = fd.readline().strip()
+            while line:
+                line = fd.readline().strip()
+                if (".V" in line):
+                    # partition des elements dans chaque ligne
+                    verb = line.split(".")[0]
+                    grammaire = line.split(".")[1]
+                    lemme = verb.split(",")[1]
+                    forme_f = verb.split(",")[0]
+                    # ajoute dans le fichier .csv
+                    phrase = lemme + "\t" + forme_f + '\t' + grammaire + '\n'
+                    phrase_muet = "*" + lemme + "\t" + forme_f + '\t' + grammaire + '\n'
+                    if(lemme in h_aspire):
+                        output_f.write(phrase_muet)
+                    else:
+                        output_f.write(phrase)
+
+
